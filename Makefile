18c68ff307faa2f80f0d869113be6d3e8697e6f4
Ester Tina <etina@2-G-2.42heilbronn.de>
2024-10-12 14:05:21 +0200

    ft_toupper

--- Makefile
+++ Makefile
@@ -6,7 +6,7 @@
 #    By: etina <etina@student.42.fr>                +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2024/10/11 19:41:59 by etina             #+#    #+#              #
-#    Updated: 2024/10/12 12:52:40 by etina            ###   ########.fr        #
+#    Updated: 2024/10/12 13:35:18 by etina            ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -16,7 +16,7 @@ CC = gcc
 
 CFLAGS = -Wall -Wextra -Werror
 
-SRCS = ft_strlen.c ft_isprint.c ft_isdigit.c ft_isascii.c ft_isalpha.c ft_isalnum.c
+SRCS = ft_strlen.c ft_isprint.c ft_isdigit.c ft_isascii.c ft_isalpha.c ft_isalnum.c ft_toupper.c
 
 OBJS = $(SRCS:.c=.o)
 
--- ft_toupper.c
+++ ft_toupper.c
@@ -1,26 +1,25 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_toupper.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: etina <etina@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/10/11 19:56:09 by etina             #+#    #+#             */
-/*   Updated: 2024/10/12 13:36:03 by etina            ###   ########.fr       */
+/*   Created: 2024/10/12 13:36:43 by etina             #+#    #+#             */
+/*   Updated: 2024/10/12 14:03:00 by etina            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
-# define LIBFT_H
+int	ft_toupper(int c)
 
-# include <stddef.h>
-
-size_t	ft_strlen(const char *s);
-int		ft_isprint(int c);
-int		ft_isdigit(int i);
-int		ft_isascii(int c);
-int		ft_isalpha(int c);
-int		ft_isalnum(int c);
-int		ft_toupper(int c);
-
-#endif
+{
+	if (c >= 97 && c <= 122)
+	{
+		c -= 32;
+		return (c);
+	}
+	else
+	{
+		return (c);
+	}
+}
