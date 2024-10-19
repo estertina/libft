a7fdc8af896a58a649265717e40a17a8ee3479b0
Ester Tina <etina@2-D-1.42heilbronn.de>
2024-10-19 21:12:44 +0200

    ft_memchr

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: etina <etina@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/11 19:56:09 by etina             #+#    #+#             */
-/*   Updated: 2024/10/19 16:43:17 by etina            ###   ########.fr       */
+/*   Updated: 2024/10/19 19:45:12 by etina            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,4 +34,5 @@ void	*ft_memset(void *b, int c, size_t len);
 void	ft_bzero(void *s, size_t n);
 void	*ft_memcpy(void *dst, const void *src, size_t n);
 void	*ft_memmove(void *dst, const void *src, size_t len);
+void	*ft_memchr(const void *s, int c, size_t n);
 #endif
