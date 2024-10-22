cb73e12c78a28c3dfb91d46125c0a3c4036eedd3
Ester Tina <etina@2-D-1.42heilbronn.de>
2024-10-22 17:07:50 +0200

    ft_calloc

---
+++ ft_calloc.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_calloc.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: etina <etina@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/22 16:30:07 by etina             #+#    #+#             */
+/*   Updated: 2024/10/22 17:05:24 by etina            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_calloc(size_t count, size_t size)
+{
+	size_t		i;
+	char		*str;
+
+	i = 0;
+	str = malloc(count * size);
+	if (str == NULL)
+		return (NULL);
+	while (str[i])
+	{
+		str[i] = 0;
+		i++;
+	}
+	return (str);
+}
